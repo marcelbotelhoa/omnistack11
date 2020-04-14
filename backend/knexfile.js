86296babe9ab88a06ed1aa220e2243f80957975e
Marcel Botelho <marcelbotelho1@hotmail.com>
2020-04-14 09:31:14 -0300

    Initial commit

---
+++ backend/knexfile.js
@@ -0,0 +1,63 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host:     '127.0.0.1',
+      user:     'root',
+      password: '',
+      database: 'db_omnistack'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  },
+
+  test: {
+    client: 'mysql',
+    connection: {
+      host:     '127.0.0.1',
+      user:     'root',
+      password: '',
+      database: 'tst_omnistack'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
